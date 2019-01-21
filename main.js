8f5b8ae22348ba957eff37e4e546a4234afe7e09
Michael Kopinsky <Michael.Kopinsky@pennmedicine.upenn.edu>
2019-01-20 22:11:09 -0500

    Make it actually dynamic

--- main.js
+++ main.js
@@ -1,38 +1,45 @@
+let pieChart;
 window.onload = function() {
   let ctx = document.getElementById('chart-area').getContext('2d');
-  let pieChart = new Chart(ctx,{
+  pieChart = new Chart(ctx,{
       type: 'pie',
-      data: data,
+      data: {
+        datasets: [{
+          data: [randomScalingFactor(), randomScalingFactor(), randomScalingFactor()],
+          backgroundColor: [
+            "rgb(255, 99, 132)",
+            "rgb(54, 162, 235)",
+            "rgb(255, 205, 86)",
+          ],
+        }],
+        labels: []
+      },
       options: {
         animation: {
           animateRotate: false,
           animateScale: true
         },
-
       }
   });
+  document.querySelector('form').addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    updateChart();
+  });
+
+  updateChart();
+}
+const updateChart = function() {
+  pieChart.data.labels = Array.from(document.querySelectorAll('.victim')).map(item => item.value);
+  pieChart.data.datasets[0].data = [
+      randomScalingFactor(),
+      randomScalingFactor(),
+      randomScalingFactor(),
+    ];
+  pieChart.update();
+
+  return false;
 }
 const randomScalingFactor = function() {
   return Math.round(Math.random() * 100);
-};
-let data = {
-  datasets: [{
-    data: [
-      randomScalingFactor(),
-      randomScalingFactor(),
-      randomScalingFactor(),
-    ],
-    backgroundColor: [
-      "rgb(255, 99, 132)",
-      "rgb(54, 162, 235)",
-      "rgb(255, 205, 86)",
-    ],
-
-  }],
-
-  labels: [
-    'Red',
-    'Yellow',
-    'Blue'
-  ]
 };
